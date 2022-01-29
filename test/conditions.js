6a0406484ff3690c28a0005677f862c2a2f18296
dy <df.creative@gmail.com>
2022-01-28 19:45:11 -0500

    Add inner conditions

--- test/conditions.js
+++ test/conditions.js
@@ -41,6 +41,26 @@ test('conditions: short', async () => {
   delete params.a
 })
 
+test('conditions: short with insertions', async () => {
+  let el = h`<p>
+    <span :if="{{ a==1 }}">{{a}}</span>
+    <span :else-if="{{ a==2 }}">{{a}}</span>
+    <span :else >{{a}}</span>
+  </p>`
+
+  const params = templize(el, { a: 1 }, exprProcessor)
+
+  is(el.innerHTML, '<span>1</span>')
+  params.a = 2
+  is(el.innerHTML, '<span>2</span>')
+  params.a = 3
+  is(el.innerHTML, '<span>3</span>')
+  params.a = 4
+  is(el.innerHTML, '<span>4</span>')
+
+  delete params.a
+})
+
 test.todo('conditions: reactive values', async () => {
 
 })
